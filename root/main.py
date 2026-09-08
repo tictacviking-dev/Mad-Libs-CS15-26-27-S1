9c62b694dd87f5902685607ea35c8f1b3710e279
AbishekV <tictacviking@gmail.com>
2026-09-08 16:31:36 -0600

    madlibs.py
    i finished the mad libs project

---
+++ root/main.py
@@ -0,0 +1,13 @@
+noun = input("Noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb ending in -ing\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun = input("Plural noun\n")
+adjective_2 = input("Another adjective\n")
+
+message = f"Today in computer science class, Mr. Forsyth taught us how to program a(n) {noun}. First, we clicked the {adjective} button, then the computer started {verb}. Suddenly, {name} accidentally created {number} {plural_noun}! Mr. Forsyth said, \"This is the most {adjective_2} bug I've ever seen.\""
+print(message)
+
+
+
