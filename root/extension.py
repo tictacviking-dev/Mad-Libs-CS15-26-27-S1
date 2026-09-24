c0e2a0ccafa990b58d2fe0dcb36cdfa750e37679
AbishekV <tictacviking@gmail.com>
2026-09-24 14:02:30 -0600

    updated extension
    sorry i forgot

---
+++ root/extension.py
@@ -0,0 +1,10 @@
+animal = input("Animal\n")
+place = input("Place\n")
+adjective = input("Adjective\n")
+food = input("Food\n")
+number = input("Number\n")
+name = input("Name\n")
+
+story = f"One day I went to {place} and saw a {number} {animal}. It was eating {food} and looked really {adjective}. My friend {name} said, \"thats cool bruh\" Then we went home."
+
+print(story)
